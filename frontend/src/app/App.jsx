95df8492ae0c4604c4ee45abb6ac03fafd841c4c
yoshinoseiji <sij.ysn25@gmail.com>
2021-05-09 00:43:12 +0900

    Introduce HashRouter

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
