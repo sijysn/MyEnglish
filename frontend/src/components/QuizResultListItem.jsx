d735c2f465697d3614ab3d39231b9328e21c0a2a
yoshinoseiji <sij.ysn25@gmail.com>
2021-05-09 00:14:31 +0900

    Add mistake

--- frontend/src/components/QuizResultListItem.jsx
+++ frontend/src/components/QuizResultListItem.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 import Box from "@material-ui/core/Box";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 
-function QuizResultListItem({ index, word, meaning }) {
+function QuizResultListItem({ index, word, meaning, mistake }) {
   return (
     <ListItem divider>
       <Box mr={2}>
@@ -14,15 +13,24 @@ function QuizResultListItem({ index, word, meaning }) {
           primaryTypographyProps={{ component: "span", variant: "h4" }}
         />
       </Box>
+
       <Box width="30%" px={3} style={{ overflowX: "hidden" }}>
         <ListItemText
           primary={word}
           primaryTypographyProps={{ component: "span", variant: "h4" }}
         />
       </Box>
-      <Box width="65%" px={3} style={{ overflowX: "hidden" }}>
+
+      <Box width="30%" px={3} style={{ overflowX: "hidden" }}>
         <ListItemText
           primary={meaning}
+          primaryTypographyProps={{ component: "span", variant: "h4" }}
+        />
+      </Box>
+
+      <Box width="35%" px={3} style={{ overflowX: "hidden" }}>
+        <ListItemText
+          primary={typeof mistake === "number" ? mistake + 1 : mistake}
           primaryTypographyProps={{ component: "span", variant: "h4" }}
         />
       </Box>
