76fe9e4149816b4cd8e03d81bab324081edfe8b5
yoshinoseiji <sij.ysn25@gmail.com>
2021-05-09 01:14:30 +0900

    Update

--- backend/settings.py
+++ backend/settings.py
@@ -190,7 +190,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'base.User'
 
