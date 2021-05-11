b4377dd5d8b10cf69091ea503e23bccac019f5b9
yoshinoseiji <sij.ysn25@gmail.com>
2021-05-11 20:53:55 +0900

    Add ref

--- frontend/src/components/ActionButton.jsx
+++ frontend/src/components/ActionButton.jsx
@@ -3,31 +3,27 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
-function ActionButton({
-  name,
-  TypographyVariant,
-  color,
-  disabled,
-  onClick,
-  style,
-}) {
-  return (
-    <Button
-      variant="outlined"
-      color={color}
-      onClick={onClick}
-      style={style}
-      disabled={disabled}
-    >
-      <Typography
-        component="span"
-        variant={TypographyVariant}
-        style={{ overflowX: "scroll" }}
+const ActionButton = React.forwardRef(
+  ({ name, TypographyVariant, color, disabled, onClick, style }, ref) => {
+    return (
+      <Button
+        variant="outlined"
+        color={color}
+        onClick={onClick}
+        style={style}
+        disabled={disabled}
       >
-        {name}
-      </Typography>
-    </Button>
-  );
-}
+        <Typography
+          component="span"
+          variant={TypographyVariant}
+          style={{ overflowX: "scroll" }}
+          ref={ref}
+        >
+          {name}
+        </Typography>
+      </Button>
+    );
+  }
+);
 
 export default ActionButton;
