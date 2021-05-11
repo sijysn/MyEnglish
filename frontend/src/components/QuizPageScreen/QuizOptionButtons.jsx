0418bebcee6db778542bc7e43c95d0cf2ef26817
yoshinoseiji <sij.ysn25@gmail.com>
2021-05-11 20:51:22 +0900

    Initialize

---
+++ frontend/src/components/QuizPageScreen/QuizOptionButtons.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef } from "react";
+
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+
+import ActionButton from "../ActionButton";
+
+const QuizOptionButtons = React.forwardRef(
+  ({ options, judge, answerQuestion }, ref) => {
+    const option1 = useRef(null);
+    const option2 = useRef(null);
+    const option3 = useRef(null);
+
+    useEffect(() => {
+      if (!ref) return;
+
+      ref.current.option1 = option1;
+      ref.current.option2 = option2;
+      ref.current.option3 = option3;
+    }, [ref]);
+
+    return (
+      <Box display="flex" flexDirection="row" justifyContent="center">
+        {options &&
+          options.map((option, index) => (
+            <Box
+              key={option.id}
+              display="flex"
+              flexDirection="column"
+              textAlign="center"
+              margin="0 2rem"
+              width="30%"
+            >
+              <ActionButton
+                name={option.meaning}
+                TypographyVariant="h4"
+                color="primary"
+                disabled={judge ? true : false}
+                ref={index === 0 ? option1 : index === 1 ? option2 : option3}
+                onClick={answerQuestion}
+                style={{
+                  padding: "1rem 2rem",
+                }}
+              />
+
+              <Typography
+                component="span"
+                variant="h5"
+                color={judge ? "textSecondary" : "primary"}
+              >
+                {index === 0 ? "←" : index === 1 ? "↑" : "→"}
+              </Typography>
+            </Box>
+          ))}
+      </Box>
+    );
+  }
+);
+
+export default QuizOptionButtons;
